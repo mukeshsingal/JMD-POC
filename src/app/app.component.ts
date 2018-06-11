0b514f33a092ebc1376d4eb1b1260a762c888b9e
mukeshsingal <mukesh.singal@kony.com>
2018-06-12 03:37:38 +0530

    initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
+  ngOnDestroy(): void {
+  }
+  ngOnInit(): void {
+    //window.addEventListener('scroll', this.addClassToNav, true);
+  }
   title = 'app';
+  options: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.options = fb.group({
+      'fixed': false,
+      'top': 0,
+      'bottom': 0,
+    });
+  }
+
+  shouldRun = true
+  //
+  // addClassToNav(){
+  //   if(window.scrollY !=0){
+  //     document.getElementById('nav01').style.paddingBottom = "15px"
+  //     document.getElementById('nav01').style.paddingTop = "15px"
+  //   }
+  //   else{
+  //     document.getElementById('nav01').style.paddingBottom = "10px"
+  //     document.getElementById('nav01').style.paddingTop = "10px"
+  //   }
+  // }
 }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AngularFontAwesomeModule} from "angular-font-awesome";
+import {
+  MatButtonModule,
+  MatCheckboxModule, MatFormFieldControl, MatFormFieldModule, MatIcon, MatIconModule, MatInputModule, MatSidenavModule,
+  MatToolbar,
+  MatToolbarModule
+} from "@angular/material";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    MatInputModule,
+    BrowserModule,
+    AngularFontAwesomeModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatCheckboxModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
